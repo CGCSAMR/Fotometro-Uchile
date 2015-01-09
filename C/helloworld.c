7aa7d0eb44ba66657b51706e0fb14d9be282d780
FelipeToledo <ftoledo.bit@gmail.com>
2015-01-09 15:46:16 -0300

    Se abre carpeta de C

---
+++ C/helloworld.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sumar (int a, int b)
+{
+	return a+b;
+}
+
+int main ()
+{
+	int num = sumar (10 , 12);
+	
+	printf("numero %d \n", num);
+	
+	int i;
+	
+	for( i = 0; i < 1 ; i++ )
+		int a = 2;
+	
+	return 0;
+}
+
+funcion(i++)
+
+funcion(++i)
+
+i++
+
+++i
